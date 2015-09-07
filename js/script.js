533f3b30ac2d2895d9934874c7b952648bef26e2
Jason Thomas <nanoman689@gmail.com>
2015-09-07 15:06:38 -0700

    Better User Experience.
    -Added the button
    -Second screen does not seem to show up correctly.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 angular.module('myApp', [])
-    .constant('VERSION', 1.1)
-    .controller('MyCtrl', function(VERSION, $scope) {
-        $scope.version = VERSION;
-    });
+  .controller('myCtrl', function($scope){
+    "use strict";
+    $scope.words = [
+    {word: 'Name', placeholder:'name'},
+    {word: 'jobTitle', placeholder:'job title'},
+    {word: 'tediousTask', placeholder:'tedious task'},
+    {word: 'dirtyTask', placeholder:'dirty task'},
+    {word: 'celebrity', placeholder:'celebrity'},
+    {word: 'uselessSkill', placeholder:'useless skill'},
+    {word: 'obnoxiousCelebrity', placeholder:'obnoxious Celebrity'},
+    {word: 'hugeNumber', placeholder:'huge number'},
+    {word: 'adjective', placeholder:'adjective'}
+    ];
+
+    $scope.madLib = true;
+
+    $scope.submit = function(){
+      var validForm = $scope.myForm.$valid;
+      var validName = $scope.myForm.myName.$valid;
+      var validJobTitle = $scope.myForm.myJobTitle.$valid;
+      var validTediousTask= $scope.myForm.myTediousTask.$valid;
+      var validDirtyTask = $scope.myForm.myDirtyTask.$valid;
+      var validCelebrity = $scope.myForm.myCelebrity.$valid;
+      var validUselessSkill = $scope.myForm.myUselessSkill.$valid;
+      var validObnoxiousCelebirty = $scope.myForm.myObnoxiousCelebirty.$valid;
+      var validHugeNumber = $scope.myForm.myHugeNumber.$valid;
+      var validAdjective = $scope.myForm.myAdjective.$valid;
+
+      if(validForm || validName || validJobTitle || validTediousTask || validDirtyTask || validCelebrity || validUselessSkill || validObnoxiousCelebirty || validHugeNumber || validAdjective){
+        // Hide Inputs if all the fields are filled.
+        $scope.formInputs = true;
+        $scope.madLib = false;
+      }
+    };
+    // Reset Button
+    $scope.startOver = function(){
+      $scope.madLib = true;
+      $scope.formInputs = false;
+    };
+  })
